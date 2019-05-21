51bd7015b61038251671c48a0da8b98b746718f7
VladimirTi <tytmes@mail.ru>
2019-05-21 10:56:29 +0300

    button added

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const html = document.querySelector('html');
-let counter = sessionStorage.getItem('count') ? sessionStorage.getItem('count') : 0;
-const htmlCount = document.createElement('div')
+let counter = sessionStorage.getItem('count');
+if(!counter) {
+  counter = 0;
+}
+const htmlCount = document.createElement('div');
 
 html.addEventListener('click', () => {
-  counter++
-  sessionStorage.setItem('count', `${counter}`)
-  console.log(counter)
-  htmlCount.innerHTML = `${counter}`
-  document.querySelector('body').append(htmlCount)
+  counter++;
+  htmlCount.innerHTML = `${counter}`;
+  document.querySelector('body').prepend(htmlCount);
 });
 
+window.addEventListener('beforeunload', () => {
+  sessionStorage.setItem('count', `${counter}`);
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  counter = 0;
+});
