eb2b9a8576ee7eaa89419743692dd0d811a1131b
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-05-23 22:17:31 +0300

    solution

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let clicks = document.querySelector('#count-clicks');
 let count = +sessionStorage.getItem('click') || 0;
+
+ window.addEventListener('load', () => {
+    clicks.textContent = count;
+ });
+
 window.addEventListener('click', () => {
     count += 1;
     clicks.textContent = count;
