a2448fa4a8eaf3f7d7c2be5345b801c127399361
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-05-24 17:35:18 +0300

    'Solution24/05/19'

--- src/js/main.js
+++ src/js/main.js
@@ -4,32 +4,14 @@ render(counterLine,  counter);
 
 document.addEventListener('DOMContentLoaded', function() {
   document.addEventListener('click', (event)=> {
-    const element = event.target;
     ++counter;
     sessionStorage.setItem('counter', counter);
     render(counterLine,  counter);
   });
+  counterLine.classList.add('counter');
 });
 
 function render(container, counter) {
   container.innerHTML = counter;
 }
   
-counterLine.style.cssText = newFunction();
-
-function newFunction() {
-  return "borderRadius: 10px; \
-    background-color: red; \
-    border: 3px solid green; \
-    right: 20px; \
-    top: 10px; \
-    color: yellow; \
-    font-size: 50px; \
-    height: 60px; \
-    width: 100px; \
-    text-align: center; \
-    right: '20px';\
-    position: absolute; \
-    border-radius: 15px; \
-    "
-}
