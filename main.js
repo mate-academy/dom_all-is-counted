ca2df10c2bea5304598991d20f4c7e21ac17aa9d
Dmitry Batsenko <batsenkod@gmail.com>
2019-05-27 13:44:59 +0300

    fixed

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const p = document.querySelector("#clickcount");
 const storage = sessionStorage;
  
-let counter = storage.getItem("clickcount");
-if(!counter) {
-    counter = 0;
-    storage.setItem("clickcount", 0);
+let counter = storage.getItem("counter") || 0;
+ 
+const counting =() => {
+    counter++;
+	renderCounter();
 }
  
-p.innerHTML = counter;
-document.addEventListener("click", () => {
-    counter++;
-    p.innerHTML = counter;
-    storage.setItem("clickcount", counter);
-});
+ function renderCounter() {
+	 p.innerHTML = counter;
+	 storage.setItem("counter", counter);
+ }
+ 
+document.addEventListener("click", counting);
+renderCounter();
