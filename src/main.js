c5245377ac07b6f0e0de4bf0dfad181b5cb13d81
Vitalii <ilovepinkpony1@gmail.com>
2019-06-06 08:49:34 +0300

    rewrited solution

--- src/main.js
+++ src/main.js
@@ -1,34 +1,23 @@
-"use strict";
+'use strict';
 
-window.addEventListener("load", main);
+window.addEventListener('load', main);
 
-const COUNT = "counter";
+const COUNT = 'counter';
 
 function main() {
-  const countPlace = document.querySelector(".count");
-
-  numberSet(countPlace);
+  const countPlace = document.querySelector('.count');
   numberIncrement(countPlace);
 }
 
 function numberIncrement(place) {
-  let incrementedNumber = numberSet();
-
-  document.addEventListener("click", () => {
-    incrementedNumber++;
-    sessionStorage.setItem(COUNT, incrementedNumber);
-    place.textContent = incrementedNumber;
-  });
-}
-
-function numberSet(place) {
-  const number = sessionStorage.getItem(COUNT);
-
-  if (!number) {
+  try {
+    let incrementedNumber = sessionStorage.getItem(COUNT);
+    document.addEventListener('click', () => {
+      incrementedNumber++;
+      sessionStorage.setItem(COUNT, incrementedNumber);
+      place.textContent = incrementedNumber;
+    });
+  } catch {
     sessionStorage.setItem(COUNT, 0);
   }
-  if (place) {
-    place.textContent = number;
-  }
-  return number;
 }
